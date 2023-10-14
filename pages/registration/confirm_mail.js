363446a8452b004d870a77c8ae1b79845765a4f2
Oleg <dovase71@gmail.com>
2023-10-14 21:35:15 +0500

    rewrite confirm mail to functions

--- pages/registration/confirm_mail.js
+++ pages/registration/confirm_mail.js
@@ -5,34 +5,45 @@ import Profile from '../../lib/getDataFromServer'
 import {getCookie, setCookie} from 'cookies-next'
 import {Alert, CircularProgress, Snackbar} from '@mui/material'
 
-class Index extends React.Component {
+// class Index extends React.Component {
 
-    constructor(props) {
-        super(props)
-        console.log(props.router.query)
+//     constructor(props) {
+//         super(props)
+        
 
-        this.state = {
+//     }
+
+//     render() {
+        
+//     }
+
+//     componentDidMount = () => {
+//         checkPage()
+//     }
+// }
+
+const confirmMail = () => {
+    console.log(props.router.query)
+
+        const [state, setState] = useState({
             code: '',
             buttonCode: 'notValid',
             newMail: '',
             timeToCode: 40,
             mailSend: false,
-        }
+        })
 
-        this.code = setInterval(() => this.codeSecond(), 1000)
-
-    }
-
+        code = setInterval(() => codeSecond(), 1000)
 
     checkPage = () => {
-        if (!this.props.router.query.token) {
+        if (!props.router.query.token) {
             Router.replace({
                 pathname: '/registration'
             })
         }
-        this.userData = getCookie('userData')
-        if (this.userData) {
-            let reg = Profile.cookieLogin(JSON.parse(this.userData).token, JSON.parse(this.userData).password)
+        userData = getCookie('userData')
+        if (userData) {
+            let reg = Profile.cookieLogin(JSON.parse(userData).token, JSON.parse(userData).password)
             if (reg.state === 'ok') {
                 let userData = {
                     token: reg.token,
@@ -47,41 +58,41 @@ class Index extends React.Component {
         }
     }
 
-    codeSecond() {
-        this.setState({timeToCode: this.state.timeToCode - 1})
-        if (this.state.timeToCode === 1) {
-            clearTimeout(this.code)
+    const codeSecond = () => {
+        setState({...state, timeToCode: state.timeToCode - 1})
+        if (state.timeToCode === 1) {
+            clearTimeout(code)
         }
     }
 
     checkCode = (event) => {
 
-        this.setState({code: event.target.value})
+        setState({...state, code: event.target.value})
 
         const isNumeric = n => !!Number(n)
-        if (this.state.buttonCode === 'good') {
+        if (state.buttonCode === 'good') {
             //pass
         } else if (event.target.value.length === 6 && isNumeric(event.target.value)) {
-            this.setState({buttonCode: 'ok'})
+            setState({...state, buttonCode: 'ok'})
         } else {
-            this.setState({buttonCode: 'notValid'})
+            setState({...state, buttonCode: 'notValid'})
         }
     }
 
     sendNewMail = () => {
-        let mail = this.state.newMail
-        this.setState({mailSend: true})
+        let mail = state.newMail
+        setState({...state, mailSend: true})
         console.log('ok')
     }
 
-    newTimer() {
-        this.setState({timeToCode: 59})
-        this.code = setInterval(() => this.codeSecond(), 1000)
+    const newTimer = () => {
+        setState({...state, timeToCode: 59})
+        code = setInterval(() => codeSecond(), 1000)
     }
 
-    sendNewCode(state) {
+    const sendNewCode = (state) => {
         if (state === 'ok') {
-            this.setState({buttonCode: 'good'})
+            setState({...state, buttonCode: 'good'})
         }
     }
 
@@ -91,13 +102,13 @@ class Index extends React.Component {
             <div className={styles.inputDiv}>
                 <span className={styles.textOnInput}>Код с почты</span>
                 <label className={styles.inputLabel}>
-                    <input className={styles.inputField2} value={this.state.code} onChange={e => this.checkCode(e)}
-                           readOnly={this.state.buttonCode === 'good'}/>
+                    <input className={styles.inputField2} value={state.code} onChange={e => checkCode(e)}
+                           readOnly={state.buttonCode === 'good'}/>
                     <div className={styles.inputInsideButton}>
-                        {time === 0 && this.state.buttonCode !== 'good'
-                            ? <div className={styles.buttonInInput} onClick={() => this.newTimer()}><span
+                        {time === 0 && state.buttonCode !== 'good'
+                            ? <div className={styles.buttonInInput} onClick={() => newTimer()}><span
                                 className={styles.buttonInInputText}>отправить заново</span></div>
-                            : this.state.buttonCode === 'good'
+                            : state.buttonCode === 'good'
                                 ? <span className={styles.textInInput}>почта уже подтверждена</span>
                                 : <span className={styles.textInInput}>отправить заново: {time}с</span>
                         }
@@ -113,13 +124,13 @@ class Index extends React.Component {
                 <span className={styles.textOnInput}>Изменить адрес электронной почты</span>
                 <label className={styles.inputLabel}>
                     <input className={styles.inputField2}
-                           value={this.state.buttonCode === 'good' ? '' : this.state.newMail}
-                           onChange={e => this.setState({newMail: e.target.value})}/>
+                           value={state.buttonCode === 'good' ? '' : state.newMail}
+                           onChange={e => setState({...state, newMail: e.target.value})}/>
                     <div className={styles.inputInsideButton}>
-                        {send === false && this.state.buttonCode !== 'good'
-                            ? <div className={styles.buttonInInput} onClick={() => this.sendNewMail()}><span
+                        {send === false && state.buttonCode !== 'good'
+                            ? <div className={styles.buttonInInput} onClick={() => sendNewMail()}><span
                                 className={styles.buttonInInputText}>отправить заново</span></div>
-                            : this.state.buttonCode === 'good'
+                            : state.buttonCode === 'good'
                                 ? <span className={styles.textInInput}>почта уже подтверждена</span>
                                 : <div className={styles.deactiveButtonInInput}><span
                                     className={styles.deactiveButtonInInputText}>отправлено</span></div>
@@ -155,7 +166,7 @@ class Index extends React.Component {
             text = 'подтверждено'
         }
         return (
-            <div className={style} onClick={() => this.confirmMail(type)}>
+            <div className={style} onClick={() => confirmMail(type)}>
                 {
                     type === 'loading' &&
                     <CircularProgress color="inherit" size='25px'/>
@@ -173,15 +184,15 @@ class Index extends React.Component {
 
             try {
 
-                if (this.state.code) {
+                if (state.code) {
 
-                    this.setState({buttonCode: 'loading'})
+                    setState({...state, buttonCode: 'loading'})
 
                     error_time = setTimeout(() => {
                         return 1 / 0
                     }, 2000)
 
-                    let reg = await Profile.confirmMail(this.state.code, this.props.router.query.token)
+                    let reg = await Profile.confirmMail(state.code, props.router.query.token)
                     if (reg.state === 'ok') {
 
                         let userData = {
@@ -189,23 +200,23 @@ class Index extends React.Component {
                             password: reg.password
                         }
 
-                        this.setState({userData: userData})
+                        setState({...state, userData: userData})
 
                         setCookie('userData', userData)
 
-                        this.setState({error: reg.error, buttonCode: 'good'})
+                        setState({...state, error: reg.error, buttonCode: 'good'})
 
                     } else {
-                        this.setState({error: reg.error, buttonCode: 'notConfirm'})
+                        setState({...state, error: reg.error, buttonCode: 'notConfirm'})
                     }
 
 
                     } else {
-                    this.setState({error: 'Все поля должны быть заполнены'})
+                    setState({...state, error: 'Все поля должны быть заполнены'})
                 }
 
             } catch {
-                this.setState({error: 'Что-то пошло не так во время отправки данных'})
+                setState({...state, error: 'Что-то пошло не так во время отправки данных'})
             } finally {
                 clearTimeout(error_time)
             }
@@ -213,50 +224,43 @@ class Index extends React.Component {
         }
     }
 
-    render() {
-        return (
+    return (
+        <div className={styles.mainbg}>
+            <Snackbar anchorOrigin={{vertical: 'top', horizontal: 'right'}} open={state.error}
+                      autoHideDuration={3000} onClose={() => setState({...state, error: false})}>
+                <Alert severity="error">
+                    {state.error}
+                </Alert>
+            </Snackbar>
+            <fieldset className={styles.regBox}>
+                <legend className={styles.legend}>Проверьте вашу почту</legend>
+                <span className={styles.prompt}>Если вы не получили код подтверждения, отправьте запрос<br/>заново, или введите актуальную почту</span>
+                <div className={styles.regRow}>
+                    <codeInput time={state.timeToCode}/>
+                    <buttonInput type={state.buttonCode}/>
+                </div>
+                <div className={styles.regRow}>
+                    <mailInput send={state.mailSend}/>
+                </div>
+                <div className={styles.buttons1}>
+                    <div className={styles.button1} onClick={() => Router.back()}><span
+                        className={styles.buttonText}>Назад</span></div>
+                    {state.buttonCode === 'good'
+                        ? <div className={styles.button2}><span className={styles.buttonText}
+                                                                onClick={() => Router.replace({
+                                                                    pathname: '../main_screen',
+                                                                    query: state.userData
+                                                                })}>
+                            Продолжить</span>
+                        </div>
+                        : <div className={styles.deactiveButton2}><span
+                            className={styles.deactiveButtonText}>Продолжить</span></div>
+                    }
 
-            <div className={styles.mainbg}>
-                <Snackbar anchorOrigin={{vertical: 'top', horizontal: 'right'}} open={this.state.error}
-                          autoHideDuration={3000} onClose={() => this.setState({error: false})}>
-                    <Alert severity="error">
-                        {this.state.error}
-                    </Alert>
-                </Snackbar>
-                <fieldset className={styles.regBox}>
-                    <legend className={styles.legend}>Проверьте вашу почту</legend>
-                    <span className={styles.prompt}>Если вы не получили код подтверждения, отправьте запрос<br/>заново, или введите актуальную почту</span>
-                    <div className={styles.regRow}>
-                        <this.codeInput time={this.state.timeToCode}/>
-                        <this.buttonInput type={this.state.buttonCode}/>
-                    </div>
-                    <div className={styles.regRow}>
-                        <this.mailInput send={this.state.mailSend}/>
-                    </div>
-                    <div className={styles.buttons1}>
-                        <div className={styles.button1} onClick={() => Router.back()}><span
-                            className={styles.buttonText}>Назад</span></div>
-                        {this.state.buttonCode === 'good'
-                            ? <div className={styles.button2}><span className={styles.buttonText}
-                                                                    onClick={() => Router.replace({
-                                                                        pathname: '../main_screen',
-                                                                        query: this.state.userData
-                                                                    })}>
-                                Продолжить</span>
-                            </div>
-                            : <div className={styles.deactiveButton2}><span
-                                className={styles.deactiveButtonText}>Продолжить</span></div>
-                        }
-
-                    </div>
-                </fieldset>
-            </div>
-        )
-    }
-
-    componentDidMount = () => {
-        this.checkPage()
-    }
+                </div>
+            </fieldset>
+        </div>
+    )
 }
 
-export default withRouter(Index)
+export default confirmMail;
